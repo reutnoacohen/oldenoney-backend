b30d4d5ede148b4e618c48ab698ad5cffff2ff7d
reutnoacohen <reutnoa2000@gmail.com>
2026-02-15 23:33:47 +0200

    Remove .env from git tracking

---
+++ models/Lead.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const leadSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Lead = mongoose.model("Lead", leadSchema);
+
+export default Lead;
